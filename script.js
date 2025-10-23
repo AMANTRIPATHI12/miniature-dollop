b09fcad7394d46040196df690df0f5826ccf8aae
AMANTRIPATHI12 <amantripathi2244@gmail.com>
2025-10-23 09:46:13 +0530

    added an event listener to add todos

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// Getting elements from the html document to target.
 const input = document.getElementById();
 const addBtn = document.getElementById();
 const List = document.getElementById();
 
+// Saving todos in local storage;
 const saved = localStorage.getItem('todos');
 const todos = saved?JSON.parse(saved):[];
 
+// Fuction to save todos in local storage
 function savedTodo(){
     localStorage.setItem('todos', JSON.stringify(todos));
 }
 
+
 function createTodoNode(text , index){
     const li = document.createElement('li');
 
+    // added checkbox feature to check competed.
     const checkbox = document.createElement('input');
     checkbox.type = checkbox;
     checkbox.checked = !!todos.completed;
@@ -24,9 +29,34 @@ function createTodoNode(text , index){
     textSpan.style.margin = '5px 5px';
     if(todos.completed){
         textSpan.style.textDecoration = 'line-through';
+
+        // adding event listener to edit todos.
+        document.addEventListener("dblclick" , ()=>{
+            const newtext = prompt("Edit todo" , todos.text);
+            if(newtext !== null){
+                todos.text = newtext.trim();
+                textSpan.textContent = todos.text;
+                savedTodo();
+            }
+        })
+
+        // delete a todo;
+        const delBtn = document.createElement('button');
+        delBtn.textContent = "Delete";
+        delBtn.addEventListener('click',()=>{
+            todos.splice(index , 1);
+            render();
+            savedTodo();
+        })
+        li.append(checkbox);
+        li.append(textSpan);
+        li.append(delBtn);
+        return li;
     }
 }
 
+
+// fuction to render todos in your html document.
 function render(){
     List.innerHTML = "";
 
@@ -36,4 +66,17 @@ function render(){
     });
 }
 
+function addTodo(){
+    const text = input.ariaValueMax.trim();
+    if(!text){
+        return;
+    }
+
+    todos.push({text,completed : false});
+    input.value = "";
+    render();
+    savedTodo();
+}
+
+addBtn.addEventListener("click" , addTodo);
 render();
